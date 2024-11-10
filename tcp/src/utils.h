0418a00246eea6bbe28911580945eaf4bdf0919c
Joe Barnett <joebarnett876@gmail.com>
2024-11-10 08:55:19 +0000

    typo

--- tcp/src/utils.h
+++ tcp/src/utils.h
@@ -9,10 +9,10 @@ typedef struct vec {
   size_t len;
 } vec;
 
-void push_uint8_t(vec v, uint8_t **pt, uint8_t value);
+void push_uint8_t(vec v, uint8_t **ptr, uint8_t value);
 void push_uint16_t(vec v, uint8_t **ptr, uint16_t value);
 void push_uint32_t(vec v, uint8_t **ptr, uint32_t value);
 
-uint8_t take_uint8_t(vec v, uint8_t **pt);
+uint8_t take_uint8_t(vec v, uint8_t **ptr);
 uint16_t take_uint16_t(vec v, uint8_t **ptr);
 uint32_t take_uint32_t(vec v, uint8_t **ptr);
